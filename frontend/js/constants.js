f36f5264ac7123b9535bec4a7bd0fb674e8f7dcf
Your Name <you@example.com>
2022-06-07 17:28:02 -0700

    rinkby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0xd2A84Ff73e14FCf2e8a875F49f01e50aafd1dEE8"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";

7faef5cf5c69824a79f72df8e5f0bb1cb30e3cbc
Trevor Smith <trevors@skoobydude.io>
2022-06-08 10:03:11 -0700

    config

--- backend/src/config.js
+++ backend/src/config.js
@@ -170,7 +170,7 @@ const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the ro
 const ROYALTY_ADDRESS = "0x0C0B2b5db7A5324966249238a554fCC70a365297"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = ["0x0C0B2b5db7A5324966249238a554fCC70a365297"]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
 let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
